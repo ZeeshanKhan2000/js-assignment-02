1da5cc44e505d9e953a74cc312ea0cc706d921e3
ZeeshanKhan2000 <107643990+ZeeshanKhan2000@users.noreply.github.com>
2022-11-04 17:30:32 -0700

    Add files via upload

---
+++ js assignment 02/custom.js
@@ -0,0 +1,189 @@
+
+// *************************PROMPT CHAPTER *****************
+
+// var spec = prompt("Your species?",);
+
+// console.log(spec)
+
+
+
+// var abc = prompt("your name?" );
+
+// console.log(abc)
+
+
+
+// var spec = prompt ("Your species?, Human ")
+// var firstname = prompt ("Your first name");
+// var lastname = prompt ("Your last name");
+// var fathername = prompt ("Father name");
+
+
+// console.log (firstname);
+// console.log(spec);
+// console.log (lastname);
+// console.log (fathername);
+
+// var a = 20;
+// var b = 30;
+// var c = a * b ;
+
+// --c
+// --c
+// --c 
+// --c
+
+// alert (c)
+
+
+// var officer = prompt ("is officer available")
+
+// if (officer == "yes") {
+//    console.log ("Document sumitted");
+
+// }else {
+//     console.log ("wapis");
+// }
+
+
+
+
+// *********************if statement  Chapter ***********************
+
+
+
+// var x = prompt("Where does the Pope live?");
+// if (x === "Vatican") {
+// alert("Correct!");
+//  }
+
+
+//  console.log(x);
+
+
+
+//  var abc = prompt("my name is ");
+//  if (abc==="zeeshan"){
+//     alert("correct!");
+//  }
+
+
+//  console.log(abc);
+
+
+
+// **********************Comparison operators******************
+
+// var yourTicketNumber = 487208
+// if (yourTicketNumber !== 487208) {
+//      alert("Better luck next time.");
+//     }
+//     console.log (yourTicketNumber);
+
+
+
+
+
+
+// var age = prompt ("Enter your age");
+// var studentcard = prompt ("Do you have a student card");
+
+// if (age > 18 ) {
+//     console.log  ("student Allow");
+// }
+// else if () {
+//     var studentCard= prompt('')
+    
+//     .log ("Student is allowed on studentcard")
+// }else {
+    
+//     .log ("Not Allowed")
+// }
+
+
+// var student1 = +prompt (1)
+// var student2 = +prompt (2)
+// var student3 = +prompt (3)
+// var student4 = +prompt (4)
+// var student5 = +prompt (5)
+
+// console.log (student1)
+// console.log (student2)
+// console.log (student3)
+// console.log (student4)
+// console.log (student5)
+
+// var addition = student1 + student2 + student3 + student4 + student5;
+
+// var total = 1000; 
+
+// var percentage =  (addition * 100 / total )
+
+// console.log (percentage);     
+
+// if (percentage => 50 && =< 60 ){
+//     console.log (Mushkil se pass)
+// }else (){
+//     console.log ("fail")
+// }
+
+
+
+
+
+// *********************if...else and else if statements***********
+
+
+
+
+// var x = prompt("Where does the Pope live?");
+// if (x === "Vatican") {
+// alert("Correct!");
+
+// }
+// console.log(x);
+
+
+// var x = prompt("Where does the Pope live?");
+//  if (x === "Vatican") {
+//  alert("Correct!");
+//  }
+//  if (x !== "Vatican") {
+//  alert("Wrong answer");
+
+//  }
+
+
+
+// if (x === "Vatican") {
+//      alert("Correct!");
+//      }
+//      else {
+//      alert("Wrong answer");
+//      }
+    
+
+
+// //      console.log(x);
+
+
+// var correctAnswer = "Vatican";
+// if (x === correctAnswer) {
+// alert("Correct!");
+//  }
+//  else {
+//  score--;
+//  userIQ = "problematic";
+//  alert("Incorrect");
+//  }
+
+
+
+
+//  console.log(correctAnswer);
+
+
+
+
+
+// *********************************Testing sets of conditions************
